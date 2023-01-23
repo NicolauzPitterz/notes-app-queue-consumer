0bc669e082829ec9dd0fc7f3e7cfc67c6ccd09c1
Pitters Nicolaus Tulungen <nicolauzpitterz@gmail.com>
2023-01-23 11:46:25 +0800

    consumer program finished

---
+++ src/NotesService.js
@@ -0,0 +1,18 @@
+const { Pool } = require("pg");
+
+class NotesService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async getNotes(userId) {
+    const query = {
+      text: "SELECT notes.* FROM notes LEFT JOIN collaborations c ON c.note_id = notes.id WHERE notes.owner = $1 OR c.user_id = $1 GROUP BY notes.id",
+      values: [userId],
+    };
+    const { rows } = await this._pool.query(query);
+    return rows;
+  }
+}
+
+module.exports = NotesService;
