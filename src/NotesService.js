b50e1f9da703bec2d51bb9134a6d6c0b5dfdbc31
Pitters Nicolaus Tulungen <nicolauzpitterz@gmail.com>
2023-01-23 11:49:40 +0800

    change query notes service

--- src/NotesService.js
+++ src/NotesService.js
@@ -7,7 +7,10 @@ class NotesService {
 
   async getNotes(userId) {
     const query = {
-      text: "SELECT notes.* FROM notes LEFT JOIN collaborations c ON c.note_id = notes.id WHERE notes.owner = $1 OR c.user_id = $1 GROUP BY notes.id",
+      text: `SELECT notes.* FROM notes
+    LEFT JOIN collaborations ON collaborations.note_id = notes.id
+    WHERE notes.owner = $1 OR collaborations.user_id = $1
+    GROUP BY notes.id`,
       values: [userId],
     };
     const { rows } = await this._pool.query(query);
